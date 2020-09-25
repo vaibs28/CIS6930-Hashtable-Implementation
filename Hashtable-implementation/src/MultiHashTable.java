ad636cf03c0cd1cf5a7b20d0710b1948465ab8cb
Vaibhav <vsahay28@gmail.com>
2020-09-25 02:21:40 -0400

    initial commit

---
+++ Hashtable-implementation/src/MultiHashTable.java
@@ -0,0 +1,17 @@
+/*
+ * Define the multi-hashing table
+ */
+public class MultiHashTable {
+  static final int CAPACITY = 1000;
+  static final int FLOWS = 1000;
+  static final int k = 3;
+}
+
+
+/*
+ * Define the entry in the map
+ * 
+ */
+class Entry {
+
+}
